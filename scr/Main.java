e69498e5fa4ae5fccae271169e7769e18b2ead95
veenjenga <121352861+veenjenga@users.noreply.github.com>
2024-11-29 21:07:36 +0300

    Update Main.java

--- scr/Main.java
+++ scr/Main.java
@@ -4,17 +4,14 @@ public class Main {
     public static void main(String[] args) {
         BankAccount account = new BankAccount(1000);
 
-        // Create Transactions
         Calendar now = Calendar.getInstance();
         DepositTransaction deposit = new DepositTransaction(500, now, "D123");
         WithdrawalTransaction withdrawal = new WithdrawalTransaction(300, now, "W123");
 
-        // Test Deposit
         deposit.apply(account);
         deposit.printTransactionDetails();
         System.out.println("Balance after deposit: " + account.getBalance());
 
-        // Test Withdrawal
         try {
             withdrawal.apply(account);
             withdrawal.printTransactionDetails();
@@ -23,7 +20,6 @@ public class Main {
             System.out.println(e.getMessage());
         }
 
-        // Test Reverse Withdrawal
         withdrawal.reverse(account);
         System.out.println("Balance after reversal: " + account.getBalance());
     }
